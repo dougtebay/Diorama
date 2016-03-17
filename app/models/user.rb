daa0f48a846acd8004d18b28e7cebaf4c03230d1
dgilbs <danielgilberg@gmail.com>
2016-03-17 15:54:00 -0400

    adds analytics methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,36 @@ class User < ActiveRecord::Base
   has_many :collections
   has_many :tweets, through: :collections
   validates_presence_of :user_name, :email
+
+  def average_collection_length
+    total_tweets = 0.00
+    self.collections.each do |collection|
+      total_tweets += collection.tweets.count 
+    end
+    total_tweets/self.collections.count
+  end
+
+  def tweets_saved_by_username(username)
+    self.tweets.where(user_name: username)
+
+  end
+
+  def tweets_saved_by_handle(handle)
+    self.tweets.where(handle: handle)
+  end
+
+  def all_handles
+    self.tweets.map do |tweet|
+      tweet.user_handle
+    end
+  end
+
+  def find_most_common_saved
+    all_handles.group_by do |handle|
+      handle
+    end.values.max_by(&:size).first
+  end
+
+
+
 end
