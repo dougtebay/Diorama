ec3fedbd674e0a5294b79d8fcb095b0a73483728
dougtebay <dougtebay@gmail.com>
2016-03-17 19:26:12 -0400

    Add style to tweets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # byebug
     @user = User.find_by(user_name: params[:user_name])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
