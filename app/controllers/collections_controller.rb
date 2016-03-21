b19bc8d833250ba3e113d09b8d28d29fa419cd9e
HarrisonLavin <harrison.lavin@gmail.com>
2016-03-21 09:15:32 -0400

    ensures authorization works as best as possible

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -41,17 +41,17 @@ before_action :authorized_to_interact
   end
 
   def index
-    @user = User.find(session[:user_id])
     @collections = Collection.all.where(privacy: false)
     @user_collections = Collection.all.where(user_id: session[:user_id])
     # @users = @collections.all.map {|col| col.user}
+    @user = User.find(session[:user_id])
   end
 
   def destroy
     @collection = Collection.find(params[:id])
     @deleted_name = @collection.name
     @collection.destroy
-    redirect_to root_path
+    redirect_to root_path, notice: "'#{@deleted_name}' has been deleted"
   end
 
   def edit
@@ -66,7 +66,7 @@ before_action :authorized_to_interact
         @collection.save
       end
     if @collection.update(collection_params)
-      
+      binding.pry
       redirect_to @collection 
     else 
       render :edit 
@@ -82,7 +82,7 @@ before_action :authorized_to_interact
   def authorized_to_interact
     if params[:id]
       @collection=Collection.find(params[:id])
-      if current_user.id != @collection.user_id
+      if session[:user_id] != @collection.user_id
         if !(@collection.privacy != true) 
           redirect_to root_path, notice: "You are not authorized to call #{params[:action]} on #{@collection.name}" 
         end
