8e1479bc9fa246b034df985f2f08da765e333cea
HarrisonLavin <harrison.lavin@gmail.com>
2016-03-29 14:19:12 -0400

    re-routes ENV keys for Twitter API

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-keys = YAML.load_file('./config/secrets.yml')
+keys = YAML.load_file('./config/application.yml')
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, keys['CONSUMER_KEY'], keys['CONSUMER_SECRET']
+  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
 end
--- config/initializers/twitter_api.rb
+++ config/initializers/twitter_api.rb
@@ -2,12 +2,11 @@ class TwitterApi
  attr_accessor :client
 
  def initialize
-   keys = YAML.load_file('./config/secrets.yml')
-   @client = Twitter::REST::Client.new do |config|
-     config.consumer_key        = keys['CONSUMER_KEY']
-     config.consumer_secret     = keys['CONSUMER_SECRET']
-     config.access_token        = keys['ACCESS_TOKEN']
-     config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
+  @client = Twitter::REST::Client.new do |config|
+     config.consumer_key        = ENV['CONSUMER_KEY']
+     config.consumer_secret     = ENV['CONSUMER_SECRET']
+     config.access_token        = ENV['ACCESS_TOKEN']
+     config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
    end
  end
 
