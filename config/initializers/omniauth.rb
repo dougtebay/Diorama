3d120f003088cb9b27de2cdcb45d5c795e2b92b1
HarrisonLavin <harrison.lavin@gmail.com>
2016-03-29 14:20:32 -0400

    yadda

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-keys = YAML.load_file('./config/application.yml')
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
 end
