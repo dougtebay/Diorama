ffa963eaaa9f0fe974c14ae7c606b25ba4213003
HarrisonLavin <harrison.lavin@gmail.com>
2016-03-30 09:35:06 -0400

    reverts

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'],{ 
-    client_options: { ssl: { 
-        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
-        ca_path: "/etc/ssl/certs"
-    }}}
+  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
 end
