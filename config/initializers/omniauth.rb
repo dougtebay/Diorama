487a341279ac37d4549d656a0befb773eaf69758
HarrisonLavin <harrison.lavin@gmail.com>
2016-03-30 09:30:18 -0400

    changes keys etc for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'],{ 
+    client_options: { ssl: { 
+        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
+        ca_path: "/etc/ssl/certs"
+    }}
 end
