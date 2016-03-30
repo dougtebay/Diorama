beae409e57cae597ff34885ff1f68467f788294e
HarrisonLavin <harrison.lavin@gmail.com>
2016-03-30 09:31:53 -0400

    further

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     client_options: { ssl: { 
         ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
         ca_path: "/etc/ssl/certs"
-    }}
+    }}}
 end
